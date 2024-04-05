04b264d243f8afa7400ffbec3f4db968daf0031c
Netanelry <66564358+Netanelry@users.noreply.github.com>
2024-04-05 09:38:18 +0300

    Create main.go

---
+++ RESTful API/web-service-gin/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+    "encoding/json"
+    "fmt"
+)
+
+type casino struct {
+    ID     string  `json:"id"`
+    Name  string  `json:"name"`
+    Location string  `json:"location"`
+    Rank  float64 `json:"rank"`
+}
+
+// casinos slice to seed record data.
+var casinos = []casino{
+    {ID: "1", Name: "Bellagio", Location: "Las Vegas, USA", Rank: 98.8},
+    {ID: "2", Name: "The Venetian Macao", Location: "Macau, China", Rank: 86.36},
+    {ID: "3", Name: "Monte Carlo Casino", Location: "Monte Carlo, Monaco", Rank: 75},
+}
+
+// getCasinos responds with the list of all casinos as JSON.
+func getCasinos(c *gin.Context) {
+    c.IndentedJSON(http.StatusOK, casinos)
+}
